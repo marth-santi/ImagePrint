2986a507059e678019d3e62eeb2c314862c06a27
Luong Duc <54354862+marth-santi@users.noreply.github.com>
2020-10-09 16:58:38 +0700

    fix css sign in page, add upload file function for print controller

--- ImagePrint/Controllers/PrintController.cs
+++ ImagePrint/Controllers/PrintController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ImagePrint.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +11,7 @@ namespace ImagePrint.Controllers
 {
     public class PrintController : Controller
     {
+        private MyImageDBEntities db = new MyImageDBEntities();
         // GET: Print
         public ActionResult UploadImage()
         {
@@ -15,6 +19,33 @@ namespace ImagePrint.Controllers
                 return RedirectToAction("Login", "LoginCustomer");
 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadImage([Bind(Include = "ImageId,ImageName")] Image image, HttpPostedFileBase uploadImg)
+        {
+            if (Session["user"] == null)
+                return RedirectToAction("Login", "LoginCustomer");
+            var user = (Customer)Session["user"];
+
+            if (!ModelState.IsValid)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            if (uploadImg == null && uploadImg.ContentLength == 0)
+            {
+                ViewBag["Error"] = "Error uploading file";
+                return View();
+            }
+
+            image.ImageName = "/Content/image/image_print/" + user.CusId + "/" + uploadImg.FileName;
+            db.Images.Add(image);
+            db.SaveChanges();
+            ViewBag.Image = image;
+
+            string urlImage = Server.MapPath("~" + image.ImageName);
+            uploadImg.SaveAs(urlImage);
+            return View(image);
         }
     }
 }
